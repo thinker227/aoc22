932a993af8eea70d78fb58052f5c230ef82f5ed6
thinker227 <thinker0227@gmail.com>
2022-12-01 10:10:51 +0100

    Day 1 part 2

--- solutions/Day01.hs
+++ solutions/Day01.hs
@@ -1,15 +1,27 @@
 module Day01 (day01) where
 
-import Solution (Solution(Single), Answer)
+import Solution (Solution(Separate), Answer)
 import ListUtils (splitBy)
 import StringUtils (blank)
+import Data.List (sort)
 
 day01 :: Solution
-day01 = Single part1
+day01 = Separate part1 part2
 
 part1 :: String -> Answer
 part1 input = show
     $ maximum
-    $ map (sum . (map read :: [String] -> [Int]))
+    $ totalCalories input
+
+part2 :: String -> Answer
+part2 input = show
+    $ sum
+    $ take 3
+    $ reverse
+    $ sort
+    $ totalCalories input
+
+totalCalories input =
+      map (sum . (map read :: [String] -> [Int]))
     $ splitBy blank
     $ splitBy (== '\n') input
--- utility/hs/ListUtils.hs
+++ utility/hs/ListUtils.hs
@@ -7,4 +7,4 @@ splitBy' :: (a -> Bool) -> [a] -> [[a]] -> [a] -> [[a]]
 splitBy' f (x:xs) l accum = if f x
     then splitBy' f xs (l ++ [accum]) []
     else splitBy' f xs l (accum ++ [x])
-splitBy' _ _ l _ = l
+splitBy' _ _ l accum = l ++ [accum]
