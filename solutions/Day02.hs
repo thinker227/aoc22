24fb9917cfb8bb266fc5f89c20712e8e8a5fe18b
thinker227 <thinker0227@gmail.com>
2022-12-02 14:44:58 +0100

    Day 2 part 2

--- solutions/Day02.hs
+++ solutions/Day02.hs
@@ -1,14 +1,21 @@
 module Day02 where
 
-import Solution (Solution (Single), Answer)
+import Solution (Solution (Separate), Answer)
+import GHC.Stack (HasCallStack)
 
 day02 :: Solution
-day02 = Single part1
+day02 = Separate part1 part2
 
 part1 :: String -> Answer
 part1 input = show
     $ sum
     $ map (getScore . getStrategy)
+    $ lines input
+
+part2 :: String -> Answer
+part2 input = show
+    $ sum
+    $ map (getScore . getBetterStrategy)
     $ lines input
 
 getScore :: (Shape, Shape) -> Int
@@ -28,6 +35,23 @@ getScore (Scissors, s) = 3 + case s of
 getStrategy :: String -> (Shape, Shape)
 getStrategy (b : ' ' : a : _) = (charToShape a, charToShape b)
 getStrategy _ = error "Malformed input"
+
+getBetterStrategy :: String -> (Shape, Shape)
+getBetterStrategy (b : ' ' : a : _) = (whatToPick a opponent, opponent)
+    where opponent = charToShape b
+getBetterStrategy _ = error "Malformed input"
+
+whatToPick :: Char -> Shape-> Shape
+whatToPick 'X' opponent = case opponent of
+    Rock -> Scissors
+    Paper -> Rock
+    Scissors -> Paper
+whatToPick 'Y' opponent = opponent
+whatToPick 'Z' opponent = case opponent of
+    Rock -> Paper
+    Paper -> Scissors
+    Scissors -> Rock
+whatToPick _ _ = error "Malformed input"
 
 charToShape :: Char -> Shape
 charToShape 'A' = Rock
