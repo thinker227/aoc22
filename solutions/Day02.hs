893cd2157e34e9f6c66150f99db8eef0467e1ece
thinker227 <thinker0227@gmail.com>
2022-12-02 13:53:05 +0100

    Day 2 part 1

--- solutions/Day02.hs
+++ solutions/Day02.hs
@@ -7,13 +7,26 @@ day02 = Single part1
 
 part1 :: String -> Answer
 part1 input = show
+    $ sum
     $ map (getScore . getStrategy)
     $ lines input
 
-getScore :: (Shape, Shape) -> Int 
+getScore :: (Shape, Shape) -> Int
+getScore (Rock, s) = 1 + case s of
+    Rock -> 3
+    Paper -> 0
+    Scissors -> 6
+getScore (Paper, s) = 2 + case s of
+   Rock -> 6
+   Paper -> 3
+   Scissors -> 0
+getScore (Scissors, s) = 3 + case s of
+   Rock -> 0
+   Paper -> 6
+   Scissors -> 3
 
 getStrategy :: String -> (Shape, Shape)
-getStrategy (opponent : ' ' : you : _) = (charToShape opponent, charToShape you)
+getStrategy (b : ' ' : a : _) = (charToShape a, charToShape b)
 getStrategy _ = error "Malformed input"
 
 charToShape :: Char -> Shape
