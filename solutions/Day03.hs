1fba89517d5f3638847b7effc732aae3fb364999
thinker227 <thinker0227@gmail.com>
2022-12-03 12:46:33 +0100

    Factor out stuff

--- solutions/Day03.hs
+++ solutions/Day03.hs
@@ -1,27 +1,22 @@
 module Day03 (day03) where
 
-import Solution (Solution (Separate), Answer)
+import Solution (Solution (Separate, Single), Answer)
 import ListUtils (splitHalf, chunk)
 import Data.List (intersect, nub)
 import Data.Char (ord)
 
-day03 :: Solution
-day03 = Separate part1 part2
+day03 = Separate
+    -- Take all lines, split them in half, then intersect the halves
+    (solve id (uncurry intersect . splitHalf))
+    -- Chunk all lines by 3, then intersect them
+    (solve (chunk 3) (\[a, b, c] -> intersect c $ intersect a b))
 
-part1 :: String -> Answer
-part1 input = show
+solve fa fb input = show
     $ sum
-    $ map (priority . head . nub . uncurry intersect . splitHalf)
+    $ map (priority . head . nub . fb)
+    $ fa
     $ lines input
 
-part2 :: String -> Answer
-part2 input = show
-    $ sum
-    $ map (priority . head . nub . \[a, b, c] -> intersect c $ intersect a b)
-    $ chunk 3
-    $ lines input
-
-priority :: Char -> Int
 priority c = let n = ord c in
     if c `elem` ['a'..'z']
     then n - ord 'a' + 1
