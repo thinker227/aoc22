7222e865c06c0897a3eec5c41e443f711f0970c5
thinker227 <thinker0227@gmail.com>
2022-12-03 13:22:57 +0100

    Document utils

--- utility/hs/ListUtils.hs
+++ utility/hs/ListUtils.hs
@@ -1,7 +1,15 @@
-module ListUtils (splitBy, splitHalf, chunk, intersectAll) where
+module ListUtils (
+    splitBy,
+    splitHalf,
+    chunk,
+    intersectAll
+) where
 
 import Data.List (intersect)
 
+-- | Splits a list by a predicate.
+-- Elements matching the predicate will act as separators
+-- and will be discarded.
 splitBy :: (a -> Bool) -> [a] -> [[a]]
 splitBy f xs = splitBy' f xs [] []
 
@@ -11,13 +19,18 @@ splitBy' f (x:xs) l accum = if f x
     else splitBy' f xs l (accum ++ [x])
 splitBy' _ _ l accum = l ++ [accum]
 
+-- | Splits a list into two equal lists.
 splitHalf :: [a] -> ([a], [a])
 splitHalf xs = splitAt (length xs `div` 2) xs
 
+-- | Chunks a list by a specified size,
+-- producing a list of lists, each of which
+-- is at most the specified size.
 chunk :: Int -> [a] -> [[a]]
 chunk _ [] = []
 chunk len xs = take len xs : chunk len (drop len xs)
 
+-- | Intersects all lists in a list.
 intersectAll :: Eq a => [[a]] -> [a]
 intersectAll (a:b:xs) = a `intersect` intersectAll (b:xs)
 intersectAll [x] = x
--- utility/hs/StringUtils.hs
+++ utility/hs/StringUtils.hs
@@ -1,5 +1,6 @@
 module StringUtils where
 import Data.Char (isSpace)
 
+-- | Returns `True` for a string which only contains whitespace.
 blank :: String -> Bool
 blank = all isSpace
