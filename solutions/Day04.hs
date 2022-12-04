2ba32df9963231ff0952a1ce881845638e9118a0
thinker227 <thinker0227@gmail.com>
2022-12-04 10:34:36 +0100

    Day 4 part 2

--- solutions/Day04.hs
+++ solutions/Day04.hs
@@ -1,17 +1,18 @@
 module Day04 where
 
-import Solution (Solution (Single), Answer)
+import Solution (Solution (Separate), Answer)
 import NumUtils (readNum)
 import Data.List (sortBy, intersect)
 import ListUtils (splitBy)
 
 day04 :: Solution
-day04 = Single part1
+day04 = Separate
+    (solve (\(xs, ys) -> all (`elem` ys) xs))
+    (solve (\(xs, ys) -> not $ null $ xs `intersect` ys))
 
-part1 :: String -> Answer
-part1 input = show
+solve f input = show
     $ length
-    $ filter (\[xs, ys] -> all (`elem` ys) xs)
+    $ filter (\[xs, ys] -> f (xs, ys))
     $ map (
           sortBy (\xs ys -> compare (length xs) (length ys))
         . map toRange
