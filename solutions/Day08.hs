69df121799e59299250b2609a4d24fe049835acc
thinker227 <thinker0227@gmail.com>
2022-12-08 12:58:26 +0100

    Day 8 part 2

--- solutions/Day08.hs
+++ solutions/Day08.hs
@@ -1,11 +1,11 @@
-module Day08 (day08) where
+module Day08 (day08, scenic, scenicLine, scenicGrid) where
 
-import Solution(Solution(Single), Answer)
+import Solution(Solution(Separate), Answer)
 import Data.Char (digitToInt)
 import ListUtils (aheadTail, columns)
 
 day08 :: Solution
-day08 = Single part1
+day08 = Separate part1 part2
 
 part1 :: String -> Answer
 part1 input = show
@@ -13,6 +13,14 @@ part1 input = show
     $ filter (== False)
     $ concat
     $ gridHidden
+    $ lines input
+
+part2 :: String -> Answer
+part2 input = show
+    $ maximum
+    $ concat
+    $ scenicGrid
+    $ map (map digitToInt)
     $ lines input
 
 gridHidden xs = let
@@ -25,3 +33,16 @@ hidden xs =
         any (>= x) ahead &&
         any (>= x) tail)
     $ aheadTail xs
+
+scenicGrid xs = let
+    hor = map scenicLine xs
+    ver = columns $ map scenicLine $ columns xs
+    in zipWith (zipWith (*)) hor ver
+
+scenicLine xs =
+    map (\(ahead, x, tail) ->
+        scenic x (reverse ahead) * scenic x tail)
+    $ aheadTail xs
+
+scenic x xs =
+    min (length xs) $ length (takeWhile (< x) xs) + 1
