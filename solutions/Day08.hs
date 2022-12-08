c829a8fbc211db0462bfa087674405d06ee4ac51
thinker227 <thinker0227@gmail.com>
2022-12-08 13:05:20 +0100

    cleanup

--- solutions/Day08.hs
+++ solutions/Day08.hs
@@ -1,4 +1,4 @@
-module Day08 (day08, scenic, scenicLine, scenicGrid) where
+module Day08 (day08) where
 
 import Solution(Solution(Separate), Answer)
 import Data.Char (digitToInt)
@@ -12,32 +12,27 @@ part1 input = show
     $ length
     $ filter (== False)
     $ concat
-    $ gridHidden
+    $ gridApply hiddenLine (&&)
     $ lines input
 
 part2 :: String -> Answer
 part2 input = show
     $ maximum
     $ concat
-    $ scenicGrid
+    $ gridApply scenicLine (*)
     $ map (map digitToInt)
     $ lines input
 
-gridHidden xs = let
-    hor = map hidden xs
-    ver = columns $ map hidden $ columns xs
-    in zipWith (zipWith (&&)) hor ver
+gridApply op zip xs = let
+    hor = map op xs
+    ver = columns $ map op $ columns xs
+    in zipWith (zipWith zip) hor ver
 
-hidden xs =
+hiddenLine xs =
     map (\(ahead, x, tail) ->
         any (>= x) ahead &&
         any (>= x) tail)
     $ aheadTail xs
-
-scenicGrid xs = let
-    hor = map scenicLine xs
-    ver = columns $ map scenicLine $ columns xs
-    in zipWith (zipWith (*)) hor ver
 
 scenicLine xs =
     map (\(ahead, x, tail) ->
