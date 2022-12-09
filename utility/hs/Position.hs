705c21ae7cc216cbd6b170fdeb007fb73b1d0691
thinker227 <thinker0227@gmail.com>
2022-12-09 13:42:40 +0100

    Update Pos

--- utility/hs/Position.hs
+++ utility/hs/Position.hs
@@ -6,10 +6,12 @@ data Pos a = Pos {
     xpos :: a,
     -- | Gets the y position.
     ypos :: a }
-    deriving (Eq)
+
+instance Eq a => Eq (Pos a) where
+    (==) (Pos x1 y1) (Pos x2 y2) = x1 == x2 && y1 == y2
 
 instance (Show a) => Show (Pos a) where
-    show (Pos x y) = "(" ++ show x ++ ", " ++ show y ++ ")"
+    show (Pos x y) = "{" ++ show x ++ ", " ++ show y ++ "}"
 
 instance Functor Pos where
     fmap f (Pos x y) = Pos (f x) (f y)
@@ -33,3 +35,15 @@ instance Num a => Num (Pos a) where
 -- to the elements of two other positions.
 posCombine :: (a -> b -> c) -> Pos a -> Pos b -> Pos c
 posCombine f (Pos x1 y1) (Pos x2 y2) = Pos (f x1 x2) (f y1 y2)
+
+-- | Constructs a position from a tuple.
+fromTuple :: (a, a) -> Pos a
+fromTuple (x, y) = Pos x y
+
+-- | Constructs a tuple from a position.
+toTuple :: Pos a -> (a, a)
+toTuple (Pos x y) = (x, y)
+
+-- | Swaps the elements of a position.
+swap :: Pos a -> Pos a
+swap (Pos x y) = Pos y x
